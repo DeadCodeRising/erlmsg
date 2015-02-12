6fb36bcdcd3d84986c9f0ab78a3f7c0f48911ffd
marius herrin <mariusherring@gmail.com>
2015-02-12 21:32:27 +0100

    started on send

--- server.erl
+++ server.erl
@@ -1,5 +1,6 @@
 -module(server).
 -export([start/0]).
+-export([prn/1]).
 
 start() -> spawn(fun init/0).
 
@@ -9,15 +10,33 @@ loop(Clients) ->
   receive
     {new_con, {Username, Pid}} ->
       io:format("*** NEW CONNECTION ***~n"),
-      loop([{Username, Pid} | clients]);
-    {send, Msg} ->
+      loop([{Username, Pid} | Clients]);
+    {send, {Rec, Msg}} ->
       io:format("*** SENDING MSG ***~n"),
-      loop(clients);
+      send(Msg, findReceiver(Rec, Clients)),
+      loop(Clients);
+    {prn} ->
+      prn(Clients);
     _ ->
       io:format("*** UNKNOWN CMD ***~n"),
-      loop(clients)
+      loop(Clients)
   end.
 
-  %send({Rec, Msg}, Clients) ->
-%    io:format("*** UNKNOWN CMD ***~n")
-%  end.
+prn([{Username, Pid} | T]) ->
+  io:format("- " ++ Username ++ "@" ++ Pid ++ "~n"),
+  prn(T);
+prn([]) ->
+  ok.
+
+
+send(Msg, [{Rec, Pid}]) ->
+  io:format("Sending to " ++ Rec ++ "@" ++ Pid ++ "~n"),
+  io:format(Msg ++ "~n");
+send(_, []) ->
+  io:format("Couldn't find the receiver").
+
+findReceiver(Rec, Clients) ->
+  lists:filter(fun(C) -> getUsername(C) == Rec end, Clients).
+
+getUsername({Username, _}) ->
+  Username.
