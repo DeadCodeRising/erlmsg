d75b60a5a5ec6b77d4e4096f779d417b2015335b
marius herrin <mariusherring@gmail.com>
2015-05-26 20:32:53 +0200

    added escript

---
+++ main.erl
@@ -0,0 +1,14 @@
+#!/usr/bin/env escript
+%% -*- erlang -*-
+%%! -smp enable -sname factorial -mnesia debug verbose
+main([]) ->
+  S = server:start(),
+  C1 = client:connect("Sam",S),
+  C1 ! {send, "Hi, anyone here?"},
+  C2 = client:connect("Mia",S),
+  C3 = client:connect("Luke",S),
+  timer:sleep(1000),
+  C2 ! {send, "Hello!"},
+  timer:sleep(1000),
+  C3 ! disconnect,
+  timer:sleep(1000).
